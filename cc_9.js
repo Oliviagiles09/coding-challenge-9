371cdf540e48a5f8c7f291fafdd739d0581ffe3c
Oliviagiles09 <giles11@usf.edu>
2025-02-23 22:02:05 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -21,3 +21,27 @@ const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
 
 console.log(emp1.getDetails());
 console.log(emp1.calculateAnnualSalary());
+
+// Task 2
+
+class Manager extends Employee{
+  constructor(name, id, department, salary, teameSize) {
+    super(name, id, department, salary);
+    this.teamSize = teamSize;
+  };
+
+getDetails() {
+  return 'Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}';
+};
+  calculateBonus() {
+    return super.calculateAnnualSalary() * 0.1;
+  };
+  calculateAnnualSalary() {
+    return super.calculateAnnualSalary() + this.calculateBonus();
+  };
+}
+
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+
+console.log(mgr1.getDetails());
+console.log(mgr1.calculateBonus());
