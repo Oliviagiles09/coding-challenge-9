628d2dfb17c653e1a4812695dcfbac9274efa422
Oliviagiles09 <giles11@usf.edu>
2025-02-23 21:52:00 -0500

    Task 1 - Created Employee Class

---
+++ cc_9.js
@@ -0,0 +1,23 @@
+// Task 1 
+
+class employee {
+  constructor(name, id, department, salary) {
+    this.name = name;
+    this.id = id;
+    this.department = department;
+    this.salary = salary;
+  };
+
+getDetails() {
+  return 'Employee: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}'; 
+};
+
+calculateAnnualSalary() {
+  return this.salary * 12;
+};
+};
+
+const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
+
+console.log(emp1.getDetails());
+console.log(emp1.calculateAnnualSalary());
