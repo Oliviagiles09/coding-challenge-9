07ad0a3674a6266f7dcf4269cf5e0d51c30d5dc0
Oliviagiles09 <giles11@usf.edu>
2025-02-23 22:16:51 -0500

    Task 3 - Created Company Class

--- cc_9.js
+++ cc_9.js
@@ -45,3 +45,29 @@ const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
 
 console.log(mgr1.getDetails());
 console.log(mgr1.calculateBonus());
+
+// Task 3
+class Company {
+  constructor(name, employees) {
+    this.name = name;
+    this.employees = [];
+  };
+
+addEmployee(employee) {
+  this.employees.push(employee);
+}
+  listEmployees() {
+    this.employees.forEach(employee => {console.log(employee.getDetails())});
+  }
+  calculateTotalPayroll() {
+    return this.employees.reduce((total, employee) => total + employee.calculateAnnualSalary(), 0);
+  }
+  promoteToManager (employee, teamSize) {
+    const index = this.employees.indexOf(employee);
+    this.employees[index] = new Manager(employee.name, employee.id, employee.department, employee.salary, teamSize);
+  }
+}
+const company = new Company("TechCorp");
+company.addEmployee(emp1);
+company.addEmployee(mge1);
+company.listEmployees();
